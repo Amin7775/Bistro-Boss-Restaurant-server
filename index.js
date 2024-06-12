8bfc4b3be29c3264fe96936f0f88cf9c69cadc5c
Amin7775 <amin.dpi24@gmail.com>
2024-06-12 14:43:06 +0600

    Added delete carts item api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 // mongodb - start
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.x4cetjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,6 +53,13 @@ async function run() {
     app.post('/carts', async(req,res)=>{
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
+    })
+
+    app.delete('/carts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
 
