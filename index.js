86d3a17ca00c26f9ea409012045fb155d4857209
Amin7775 <amin.dpi24@gmail.com>
2024-06-14 15:30:49 +0600

    Make User Api Secure Using Verify Admin middleware

--- index.js
+++ index.js
@@ -56,8 +56,20 @@ async function run() {
       });
     }
 
+    // verify admin middleware : use it after verifyToken
+    const verifyAdmin = async(req,res,next) =>{
+      const email = req.decoded?.email;
+      const query = {email : email}
+      const user = await userCollection.findOne(query)
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next()
+    }
+
     // user related apis
-    app.get('/users',verifyToken, async(req,res)=>{
+    app.get('/users',verifyToken,verifyAdmin, async(req,res)=>{
       // console.log(req.headers)
       const result = await userCollection.find().toArray()
       res.send(result)
@@ -89,14 +101,14 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/users/:id', async(req,res)=>{
+    app.delete('/users/:id',verifyToken,verifyAdmin, async(req,res)=>{
       const id = req.params.id;
       const query = {_id : new ObjectId(id)}
       const result = await userCollection.deleteOne(query)
       res.send(result)
     })
 
-    app.patch('/users/admin/:id', async(req,res)=>{
+    app.patch('/users/admin/:id',verifyToken,verifyAdmin, async(req,res)=>{
       const id =req.params.id;
       const filter = {_id : new ObjectId(id)}
       const updatedDoc = {
